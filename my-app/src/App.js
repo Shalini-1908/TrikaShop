d6be74652410b76dff9126d06e05657f89713fec
Shalini-1908 <shalini.s@trikatechnologies.com>
2025-01-29 16:29:44 +0530

    removed home from app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import "./App.css";
 
 import Home from "./Components/Home";
+import AllRoutes  from "../src/Components/Routes/AllRoutes";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+
+      <AllRoutes/>
     </div>
   );
 }
